176e33a25e8d61c4712c56c2d605000486597352
Akshaya1932002 <akshayakarnan02@gmail.com>
2025-09-03 10:46:48 +0530

    addarr.c

--- array/addarray.c
+++ array/addarray.c
@@ -6,16 +6,19 @@ void main()
     scanf("%d",&row);
     printf("enter the column size");
     scanf("%d",&column);
+
     int arr[row][column];
     int a,b;
     printf("Enter the array elements:\n");
-    for(a=0;a<row;a++)
+
+    for(a=0;a<row;a++) //this loop is for add the value of the matrix from R11 TO R13 and C11 to C13
     {
         for(b=0;b<column;b++)
         {
             scanf("%d",&arr[a][b]);
         }
     }
+    
     int sum=0;
     for(a=0;a<row;a++)
     {
