f19c79d3c5e23ea2520095db05c147460fded0fd
Akshaya1932002 <akshayakarnan02@gmail.com>
2025-11-03 22:05:10 +0530

    palindrome.c

--- welcome/practice.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("I LIKE BLACKPINK\n");          //this is my first c program
-    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n"); /*so this is how i practice by myself and improve my skills and knowledge*/
-    printf("\"Live And Let Live\" - Akshaya\n"); // quote
-    return 0;
-}
-
